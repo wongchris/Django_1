e354e55fe7ffefc8e051df8806469241b671f9e1
chris.wong <Cw1409cw>
2020-11-18 16:14:04 +0800

    Add Hello Second World!

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print('Hello World!')
+    print("Hello Second World!")
 
 
 # Press the green button in the gutter to run the script.
